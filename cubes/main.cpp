11087afdadb4e749ccdc70308a8038f71f5e4776
Fred <guru.three@gmail.com>
1994-03-13 05:31:00 +0000

    color & region update

--- cubes/main.cpp
+++ cubes/main.cpp
@@ -9,8 +9,12 @@
 
 #define WINDOWX 40
 #define WINDOWY 40+32
-#define XRESOLUTION 340
-#define YRESOLUTION 240
+//#define XRESOLUTION 340
+//#define YRESOLUTION 240
+#define XRESOLUTION 560
+#define YRESOLUTION 368
+
+#define USEOFFSCREEN
 
 void InitToolbox(void);
 
@@ -40,9 +44,13 @@ void rotateCube(Vector3 *cube, Vector3 *rotatedCube, Vector3 *centre, float xang
 	}
 }
 
-void drawCube(Vector3 *rotatedCube)
+void drawCube(Vector3 *rotatedCube, Boolean color)
 {
 	// front face
+	if (color)
+	{
+		ForeColor(redColor);
+	}
 	MoveTo(rotatedCube[0].x, rotatedCube[0].y);
 	LineTo(rotatedCube[1].x, rotatedCube[1].y);
 	LineTo(rotatedCube[2].x, rotatedCube[2].y);
@@ -50,6 +58,10 @@ void drawCube(Vector3 *rotatedCube)
 	LineTo(rotatedCube[0].x, rotatedCube[0].y);
 
 	// back face
+	if (color)
+	{
+		ForeColor(blueColor);
+	}
 	MoveTo(rotatedCube[4].x, rotatedCube[4].y);
 	LineTo(rotatedCube[5].x, rotatedCube[5].y);
 	LineTo(rotatedCube[6].x, rotatedCube[6].y);
@@ -57,6 +69,10 @@ void drawCube(Vector3 *rotatedCube)
 	LineTo(rotatedCube[4].x, rotatedCube[4].y);
 	
 	// connecting bits
+	if (color)
+	{
+		ForeColor(greenColor);
+	}
 	MoveTo(rotatedCube[0].x, rotatedCube[0].y);
 	LineTo(rotatedCube[4].x, rotatedCube[4].y);
 	MoveTo(rotatedCube[1].x, rotatedCube[1].y);
@@ -67,10 +83,32 @@ void drawCube(Vector3 *rotatedCube)
 	LineTo(rotatedCube[7].x, rotatedCube[7].y);
 }
 
+void cubeBounds(Vector3 *rotatedCube, RgnHandle rgn)
+{
+	int i;
+	int left = 1000, right = 0, top = 1000, bottom = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		if (rotatedCube[i].x < left)
+			left = rotatedCube[i].x;
+		if (rotatedCube[i].x > right)
+			right = rotatedCube[i].x;
+			
+		if (rotatedCube[i].y < top)
+			top = rotatedCube[i].y;
+		if (rotatedCube[i].y > bottom)
+			bottom = rotatedCube[i].y;
+	}
+
+	SetRectRgn(rgn, left-10, top-10, right+10, bottom+10);
+}
+
 void writeTPF(char buffer[], unsigned char TPF)
 {
 	sprintf(buffer, "TPF: %hu", TPF);
 	CtoPstr(buffer);
+	ForeColor(blackColor);
 	MoveTo(7, 15);
 	DrawString((unsigned char *)buffer);
 }
@@ -85,9 +123,13 @@ void main()
 	EventRecord myEvent;
 
 	// graphics variables
+#ifdef USEOFFSCREEN
 	GDHandle onscreenDevice;
-	GWorldPtr onScreen, offScreen;
-	PixMapHandle pixels;
+	GWorldPtr onScreen, offScreen; //, mask;
+	PixMapHandle pixels; //, maskpixels;
+//	Rect tpfRect;
+	RgnHandle updateRgn, tpfRgn;
+#endif
 
 	// cube variables
 	float size = 100;
@@ -117,13 +159,28 @@ void main()
 	ShowWindow(appWindow); // bring to front
 	SetPort(appWindow);	   // make the window the quickdraw graphics target
 
+#ifdef USEOFFSCREEN
 	// front buffer
 	GetGWorld(&onScreen, &onscreenDevice);
-	// create back buffer
+	// create back buffer (pixel depth 0 reuses screen depth)
 	NewGWorld(&offScreen, 0, &(appWindow->portRect), NULL, NULL, 0);
+	// "ctSeed slamming" to avoid having to remap colors on blit
+	(*( (*( offScreen->portPixMap ) )->pmTable) )->ctSeed = 
+		(*( (*( (*(GetGDevice()))->gdPMap) )->pmTable) )->ctSeed;
 	// get memory of back buffer & lock to keep from being destroyed
 	pixels = GetGWorldPixMap(offScreen);
 	LockPixels(pixels);
+	
+//	NewGWorld(&mask, 1, &(appWindow->portRect), NULL, NULL, 0);
+//	maskpixels = GetGWorldPixMap(offScreen);
+//	LockPixels(maskpixels);
+	
+//	SetRect(&tpfRect, 0, 0, 80, 40); // for covering up old TPF
+
+tpfRgn = NewRgn();
+	SetRectRgn(tpfRgn, 0, 0, 80, 40);
+	
+#endif
 
 	running = true;
 	while (running == true)
@@ -157,31 +214,64 @@ void main()
 		rotateCube(cube, rotatedCube, &centre, xangle, yangle, zangle);
 
 		// render
+#ifdef USEOFFSCREEN
 		SetGWorld(offScreen, NULL);
+#endif
+		// erase the bound of port rect on the current port
 		EraseRect(&appWindow->portRect);
-		drawCube(rotatedCube);
+//		EraseRect(&tpfRect);
+		drawCube(rotatedCube, true);
 		writeTPF(buffer, TPF);
 		
+		ForeColor(blackColor);
 		MoveTo(7, YRESOLUTION - 7);
 		#if mc68881
 			DrawString("\pFPU");
 		#else
 			DrawString("\pNO FPU");
 		#endif
+		
+//		SetGWorld(mask, NULL);
+//		EraseRect(&appWindow->portRect);
+//		InvertRect(&tpfRect);
+//		drawCube(rotatedCube, false);
+
+updateRgn = NewRgn();
+//RectRgn(updateRgn, &tpfRect);
+cubeBounds(rotatedCube, updateRgn);
+UnionRgn(updateRgn, tpfRgn, updateRgn);
 
 		// copy back buffer to front, which will also trigger a display refresh
+#ifdef USEOFFSCREEN
 		SetGWorld(onScreen, onscreenDevice);
+//		EraseRect(&appWindow->portRect);
 		CopyBits(&(((GrafPtr)offScreen)->portBits),
 				 &(((GrafPtr)onScreen)->portBits),
 				 &(offScreen->portRect),
 				 &(onScreen->portRect),
-				 srcCopy, NULL);
+				 srcCopy, updateRgn);
+//				 srcCopy, NULL);
+//		CopyMask(&(((GrafPtr)offScreen)->portBits),
+//				 &(((GrafPtr)mask)->portBits),
+//				 &(((GrafPtr)onScreen)->portBits),
+//				 &(offScreen->portRect),
+//				 &(mask->portRect),
+//				 &(onScreen->portRect));
+#endif
+
+
+DisposeRgn(updateRgn);
 
 		TPF = TickCount() - last;
 
 	} // while running
 
+#ifdef USEOFFSCREEN
+	DisposeRgn(tpfRgn);
+//	UnlockPixels(maskpixels);
+//	DisposeGWorld(mask);
 	UnlockPixels(pixels);
 	DisposeGWorld(offScreen);
+#endif
 	DisposeWindow(appWindow);
 }

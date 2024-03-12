d0673151b3c1dc56b4279629842454751f0a66f6
Fred <guru.three@gmail.com>
2024-03-12 05:11:00 +0000

    rotation & rendering

--- cubes/main.cpp
+++ cubes/main.cpp
@@ -7,6 +7,8 @@
 
 #include "vectormath2.h"
 
+#define WINDOWX 40
+#define WINDOWY 40+32
 #define XRESOLUTION 340
 #define YRESOLUTION 240
 
@@ -14,131 +16,172 @@ void InitToolbox(void);
 
 void InitToolbox()
 {
-	InitGraf((Ptr) &qd.thePort);
+	InitGraf((Ptr)&qd.thePort);
 	InitFonts();
 	InitWindows();
 	InitMenus();
-	FlushEvents(everyEvent,0);
+	FlushEvents(everyEvent, 0);
 	TEInit();
 	InitDialogs(0L);
 	InitCursor();
 }
 
+void rotateCube(Vector3 *cube, Vector3 *rotatedCube, Vector3 *centre, float xangle, float yangle, float zangle)
+{
+	int i;
+	Matrix3 rot = Matrix3::getRotationMatrix(xangle, yangle, zangle);
+	for (i = 0; i < 8; i++)
+	{
+		rotatedCube[i] = rot.preMultiply(cube[i]);
+		rotatedCube[i] = rotatedCube[i].add(*centre);
+		rotatedCube[i] = rotatedCube[i].scale(40.0f / (-rotatedCube[i].z / 8.0f + 40.0f));
+		rotatedCube[i].x = rotatedCube[i].x + XRESOLUTION / 2;
+		rotatedCube[i].y = rotatedCube[i].y + YRESOLUTION / 2;
+	}
+}
+
+void drawCube(Vector3 *rotatedCube)
+{
+	// front face
+	MoveTo(rotatedCube[0].x, rotatedCube[0].y);
+	LineTo(rotatedCube[1].x, rotatedCube[1].y);
+	LineTo(rotatedCube[2].x, rotatedCube[2].y);
+	LineTo(rotatedCube[3].x, rotatedCube[3].y);
+	LineTo(rotatedCube[0].x, rotatedCube[0].y);
+
+	// back face
+	MoveTo(rotatedCube[4].x, rotatedCube[4].y);
+	LineTo(rotatedCube[5].x, rotatedCube[5].y);
+	LineTo(rotatedCube[6].x, rotatedCube[6].y);
+	LineTo(rotatedCube[7].x, rotatedCube[7].y);
+	LineTo(rotatedCube[4].x, rotatedCube[4].y);
+	
+	// connecting bits
+	MoveTo(rotatedCube[0].x, rotatedCube[0].y);
+	LineTo(rotatedCube[4].x, rotatedCube[4].y);
+	MoveTo(rotatedCube[1].x, rotatedCube[1].y);
+	LineTo(rotatedCube[5].x, rotatedCube[5].y);
+	MoveTo(rotatedCube[2].x, rotatedCube[2].y);
+	LineTo(rotatedCube[6].x, rotatedCube[6].y);
+	MoveTo(rotatedCube[3].x, rotatedCube[3].y);
+	LineTo(rotatedCube[7].x, rotatedCube[7].y);
+}
+
+void writeTPF(char buffer[], unsigned char TPF)
+{
+	sprintf(buffer, "TPF: %hu", TPF);
+	CtoPstr(buffer);
+	MoveTo(7, 15);
+	DrawString((unsigned char *)buffer);
+}
+
+
 void main()
 {
-	WindowPtr newWindow;
+	// window variables
+	WindowPtr appWindow;
 	Rect windowRect;
-	Boolean running, result;
+	Boolean running;
 	EventRecord myEvent;
-	
-	unsigned long int count;
-	unsigned char smallint;
-	char text[100];
-	
+
+	// graphics variables
 	GDHandle onscreenDevice;
-	GWorldPtr onscreen, offscreen;
+	GWorldPtr onScreen, offScreen;
 	PixMapHandle pixels;
-	
+
+	// cube variables
+	float size = 100;
+	float xangle = 30, yangle = 0, zangle = 45;
+	float dxangle = 0.01, dyangle = 0.0, dzangle = 0.01;
+	Vector3 cube[8] = {
+		{-size / 2, -size / 2,  size / 2},
+		{ size / 2, -size / 2,  size / 2},
+		{ size / 2,  size / 2,  size / 2},
+		{-size / 2,  size / 2,  size / 2},
+		{-size / 2, -size / 2, -size / 2},
+		{ size / 2, -size / 2, -size / 2},
+		{ size / 2,  size / 2, -size / 2},
+		{-size / 2,  size / 2, -size / 2}};
+	int i;
+	Vector3 centre = {0, 0, 0};
+	Vector3 rotatedCube[8];
+
+	// benchmark variables
+	unsigned char TPF = 0; // ticks per frame
+	char buffer[100];
 	InitToolbox();
-	
-	
-	SetRect(&windowRect, 40, 40+32, XRESOLUTION, YRESOLUTION);	
-	newWindow = NewWindow(0L, &windowRect, "\plines", true, movableDBoxProc, (WindowPtr) -1L, 1, 0);
-	ShowWindow(newWindow); // bring to front
-	SetPort(newWindow); // make the window the quickdraw graphics target
+	unsigned long int last;
+
+	SetRect(&windowRect, WINDOWX, WINDOWY, WINDOWX + XRESOLUTION, WINDOWY + YRESOLUTION);
+	appWindow = NewWindow(0L, &windowRect, "\pSpinning Cube Benchmark", true, movableDBoxProc, (WindowPtr)-1L, 1, 0);
+	ShowWindow(appWindow); // bring to front
+	SetPort(appWindow);	   // make the window the quickdraw graphics target
 
 	// front buffer
-	GetGWorld(&onscreen, &onscreenDevice);
+	GetGWorld(&onScreen, &onscreenDevice);
 	// create back buffer
-	NewGWorld(&offscreen, 0, &(newWindow->portRect), NULL, NULL, 0);
+	NewGWorld(&offScreen, 0, &(appWindow->portRect), NULL, NULL, 0);
 	// get memory of back buffer & lock to keep from being destroyed
-	pixels = GetGWorldPixMap(offscreen);
+	pixels = GetGWorldPixMap(offScreen);
 	LockPixels(pixels);
-	
-	running = true;
-	while ( running == true ) {
 
-		result = WaitNextEvent(everyEvent, &myEvent, 1, NULL);
-		if (result) {
-			switch (myEvent.what) {
-			
+	running = true;
+	while (running == true)
+	{
+		last = TickCount();
+	
+		if (WaitNextEvent(everyEvent, &myEvent, 1, NULL))
+		{
+			switch (myEvent.what)
+			{
 				case activateEvt:
-					if ((myEvent.modifiers & activeFlag) == 0)  {
+					// quit when we loose focus
+					if ((myEvent.modifiers & activeFlag) == 0)
+					{
 						running = false;
 					}
 					break;
 
 				default:
 					break;
-			}					
+			}
 		}
-		
-		count++;
-		smallint = count; // cut things off and get only the <255 bit
 
-		SetGWorld(offscreen, NULL);
-		EraseRect(&newWindow->portRect);
-		MoveTo(20,20);
-		sprintf(text, "COUNT: %hu", smallint);
-		CtoPstr(text);
-		DrawString((unsigned char*)text);
+		// process cube
+		xangle += dxangle;
+		yangle += dyangle;
+		zangle += dzangle;
+		if (xangle >= 360) xangle -= 360;
+		if (yangle >= 360) yangle -= 360;
+		if (zangle >= 360) zangle -= 360;
+		rotateCube(cube, rotatedCube, &centre, xangle, yangle, zangle);
+
+		// render
+		SetGWorld(offScreen, NULL);
+		EraseRect(&appWindow->portRect);
+		drawCube(rotatedCube);
+		writeTPF(buffer, TPF);
 		
-		SetGWorld(onscreen, onscreenDevice);
-		CopyBits(&(((GrafPtr)offscreen)->portBits), 
-				 &(((GrafPtr)onscreen)->portBits), 
-				 &(offscreen->portRect),
-				 &(onscreen->portRect),
+		MoveTo(7, YRESOLUTION - 7);
+		#if mc68881
+			DrawString("\pFPU");
+		#else
+			DrawString("\pNO FPU");
+		#endif
+
+		// copy back buffer to front, which will also trigger a display refresh
+		SetGWorld(onScreen, onscreenDevice);
+		CopyBits(&(((GrafPtr)offScreen)->portBits),
+				 &(((GrafPtr)onScreen)->portBits),
+				 &(offScreen->portRect),
+				 &(onScreen->portRect),
 				 srcCopy, NULL);
-		
+
+		TPF = TickCount() - last;
+
 	} // while running
 
 	UnlockPixels(pixels);
-	DisposeGWorld(offscreen);
-	DisposeWindow(newWindow);
-	
-}
-
-// probably going to need to do TPF - ticks per frame
-
-
-
-
-
-void main2()
-{
-	Vector3 a = {1, 1, 1}, b = {2, 2, 2}, c;
-	float size = 20;
-	float xangle = 45, yangle = 60, zangle = 30;
-//	Vector3 cube[8];
-	Vector3 cube[8] = {
-		{-size/2, -size/2,  size/2},
-		{ size/2, -size/2,  size/2},
-		{ size/2,  size/2,  size/2},
-		{-size/2,  size/2,  size/2},
-		{-size/2, -size/2, -size/2},
-		{ size/2, -size/2, -size/2},
-		{ size/2,  size/2, -size/2},
-		{-size/2,  size/2, -size/2}
-	};
-	int i;
-	Matrix3 rot = Matrix3::getRotationMatrix(xangle, yangle, zangle);
-	Vector3 centre = {0, 0, 0};
-	Vector3 rotated_cube[8];
-	
-	for (i = 0; i < 8; i++)
-	{
-//		cube[i] = {1.0f, 1.0f, 1.0f};
-//		cout << i << endl;
-//		cube[i].x = i;
-//		cube[i].y = i;
-//		cube[i].z = i;
-		rotated_cube[i] = rot.preMultiply(cube[i]);
-		rotated_cube[i] = rotated_cube[i].add(centre);
-		rotated_cube[i] = rotated_cube[i].scale(40.0f / (-rotated_cube[i].z/2.0f + 40.0f));
-		rotated_cube[i].x = rotated_cube[i].x + XRESOLUTION/2;
-		rotated_cube[i].y = rotated_cube[i].y + YRESOLUTION/2;
-	}
-	
-
-
+	DisposeGWorld(offScreen);
+	DisposeWindow(appWindow);
 }
